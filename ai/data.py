d500d117ec0888d6a4b76abfcd240f000a1cfd7f
pwmax <hqwhq0@gmail.com>
2022-06-13 23:29:13 +0700

    tokenizer for python code

--- ai/data.py
+++ ai/data.py
@@ -1,91 +1,120 @@
 import time
 import os
-import tokens
+import io
+import tokenize
 import config
 import numpy as np
+from tokens import token_to_id, id_to_token
 
+"""
+150k Python Dataset  https://www.sri.inf.ethz.ch/py150
+
+"""
+# Python file in one string [str]
 def fetch(pyfile):
     ret = ''
-    
-    with open(pyfile, 'r') as file:
-        for i in file:
-            ret += i
-    
+    try:
+        with open(pyfile, 'r') as file:
+            for i in file:
+                ret += i
+    except (UnicodeDecodeError, FileNotFoundError):
+        return []
     return [ret]
- 
-def tokenize_keyword(data, keyword):
-    ret = []
-    l = len(keyword)
-    
-    for s in data:
-        ss = ''
-        lasti = 0
-        for i in range(0, len(s)-l+1):
-            if s[i:i+l] == keyword and s not in tokens.token_to_id.keys():
-                ret.append(s[lasti:i])
-                ret.append(s[i:i+l])
-                lasti = i+l
-        ret.append(s[lasti:])
 
-    return ret
+def extract_keywords(pyfile): 
+    keywords = []
+    keywords_info = []
+    with tokenize.open(pyfile) as f:
+        all_tokens = tokenize.generate_tokens(f.readline)
+        for keyword in all_tokens:
+            keywords_info.append(keyword)
+        for keyword in keywords_info:
+            keywords.append(keyword[1])
+    return keywords, keywords_info
 
-def tokenize(data):
-    for i in tokens.token_to_id.keys():
-        data = tokenize_keyword(data, i)
-    data = list(filter(lambda x: x != '', data))
+def tokenizer_processing(tokens):
+    ret_tokens = []
+    # Remove comments and ''
+    tokens = list(filter(lambda x: x != '', tokens))
+    tokens = list(filter(lambda x: x[0] != '#', tokens))
+    tokens = list(filter(lambda x: x[0:3] != "'''", tokens))
+    tokens = list(filter(lambda x: x[0:3] != '"""', tokens))
     
-    return data
+    # If token not found return []
+    for token in tokens:
+        if token in token_to_id.keys():
+            ret_tokens.append(token)
+        else:
+            l = [c for c in token if c in token_to_id.keys()]
+            if len(l) != len(token):
+                return []
+            else:
+                ret_tokens.extend(l)  
+    return ret_tokens
 
-def tokens_to_index(data):
-    if len(data) == 0:
-        return None
-    
-    ret = []
-    for i in range(len(data)):
-        try:
-            ret.append(tokens.token_to_id[data[i]])
-    
-        except KeyError:
-            return None
+# Tokenize .py file
+def tokenizer(pyfile):
+    data = fetch(pyfile)
+    if not data:
+        return []
+    tokens = []
+    keywords, _ = extract_keywords(pyfile)
+    data = data[0]
+    lasti = 0
+    for keyword in keywords:
+        i = data.find(keyword)
+        s1 = data[:i]
+        s2 = data[i:i+len(keyword)]
+        tokens.append(s1)
+        tokens.append(s2)
+        data = data[i+len(keyword):]
+    tokens = tokenizer_processing(tokens)
+    return tokens
 
-    return ret
+# String tokens to integer
+def tokens_to_id(tokens):
+    id_tokens = []
+    for token in tokens:
+        id_tokens.append(token_to_id[token])
+    return id_tokens
 
-def index_to_tokens(data):
-    ret = []
-    
-    for i in range(len(data)):
-        ret.extend(tokens.id_to_token[data[i]])
-    
-    return ret
+# Integer tokens to string
+def id_to_tokens(id_tokens):
+    ret_tokens = []
+    for i in id_tokens:
+        ret_tokens.append(id_to_token[i])
+    return ret_tokens
 
-def dataset(data, seq_len):
+# Split data on x, y (input, target) 
+def data_to_xy(data, seq_len):
     x, y = [], []
-
     for i in range(len(data)-seq_len):
         x.append(data[i:i+seq_len])
         y.append(data[i+seq_len])
-
     return x, y
-    
-def main():
-    paths = list(open('ai/data/python50k_eval.txt', 'r'))
-    x_data, y_data = [], []
 
+# Create numpy dataset
+def create_dataset(dataset_size=500000, seq_len=32):
+    paths = list(open('ai/data/python100k_train.txt', 'r'))
+    x_data, y_data = [], []
     for i in range(len(paths)): 
-        data = fetch(f'ai/data/{paths[i]}'[:-1])
-        token_data = tokenize(data)
-        index_data = tokens_to_index(token_data)
-        
-        if index_data:
-            x, y = dataset(index_data, 16)
-            print(len(x_data), len(y_data), i)
+        tokens = tokenizer(f'ai/data/{paths[i]}'[:-1])
+        if tokens:
+            tokens = tokens_to_id(tokens)
+            x, y = data_to_xy(tokens, seq_len)
+            print(len(y_data), i)
             x_data.extend(x)
             y_data.extend(y)
-        
-        if i == 400:
+        if len(y_data) >= dataset_size:
             np.save(f'{config.DATASET_PATH}x.npy', np.array(x_data, dtype=np.uint8))
             np.save(f'{config.DATASET_PATH}y.npy', np.array(y_data, dtype=np.uint8))
             break
 
 if __name__ == '__main__':
-    main()
+    #tokens = tokenizer('ai/models/grumodel.py')
+    #print(tokens)
+    #tokens = tokens_to_id(tokens)
+    #print(tokens)
+    #tokens = id_to_tokens(tokens)
+    #print(tokens)
+    #create_dataset(dataset_size=100000, seq_len=32)
